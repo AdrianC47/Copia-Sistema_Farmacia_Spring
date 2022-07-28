5b4664d88b507305527452ef40933dd95ef1c4b8
Adrian <luiscabreraucme@hotmail.com>
2022-07-28 00:04:28 -0500

    without tomcat

--- src/main/java/ups/edu/ec/Sistema_Farmacia_S/SistemaFarmaciaSApplication.java
+++ src/main/java/ups/edu/ec/Sistema_Farmacia_S/SistemaFarmaciaSApplication.java
@@ -8,12 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class SistemaFarmaciaSApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SistemaFarmaciaSApplication.class, args);
-    }
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SistemaFarmaciaSApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaFarmaciaSApplication.class, args);
     }
 
 }
